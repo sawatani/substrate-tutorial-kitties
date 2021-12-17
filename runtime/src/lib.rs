4a0d8293824e345f2d9471fa6623abcafc57945d
沢谷 邦夫 <devel@fathens.org>
2021-12-17 20:09:46 +0900

    Add skeleton of pallet-kitties

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -294,8 +294,7 @@ construct_runtime!(
 		Balances: pallet_balances,
 		TransactionPayment: pallet_transaction_payment,
 		Sudo: pallet_sudo,
-		// Include the custom logic from the pallet-template in the runtime.
-		TemplateModule: pallet_kitties,
+		SubstrateKitties: pallet_kitties,
 	}
 );
 
